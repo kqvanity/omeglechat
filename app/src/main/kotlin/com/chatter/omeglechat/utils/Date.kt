c663dfdbe3da440062035ac5192aebb868136871
polendina <solicitmind@gmail.com>
2023-11-16 14:49:36 +0200

    feat: implement the date format as messages' metadata

---
+++ app/src/main/kotlin/com/chatter/omeglechat/utils/Date.kt
@@ -0,0 +1,9 @@
+package com.chatter.omeglechat.ChatScreen
+
+import java.util.Date
+
+fun Date.dateTwelveFormat(): String {
+    val amPm = if (this.hours >= 12) "PM" else "AM"
+    val hours = if (hours > 12) hours - 12 else hours
+    return ("$hours:$minutes $amPm")
+}
